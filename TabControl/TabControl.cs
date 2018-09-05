d9dccc4337b236347c84c5ba9e72bd5fb5d48f83
Colin Kiama <colinkiama@hotmail.co.uk>
2018-09-05 19:13:17 +0100

    Discovered how to use dependency properties in runtime components. Thank you StackOverflow!

--- TabControl/TabControl.cs
+++ TabControl/TabControl.cs
@@ -19,5 +19,48 @@ namespace TabControl
         {
             this.DefaultStyleKey = typeof(TabControl);
         }
+
+        #region Properties
+
+        public ListView Tabs
+        {
+            get { return (ListView)GetValue(TabsProperty); }
+            private set { SetValue(TabsProperty, value); }
+        }
+
+        internal static DependencyProperty TabsProperty { get; } = DependencyProperty.Register("myTabs", typeof(ListView), typeof(TabControl), new PropertyMetadata(null));
+
+
+
+        public object TabsItemSource
+        {
+            get { return (object)GetValue(TabsItemSourceProperty); }
+            set { SetValue(TabsItemSourceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TabsItemSource.  This enables animation, styling, binding, etc...
+        internal static DependencyProperty TabsItemSourceProperty { get; } =
+            DependencyProperty.Register("TabsItemSource", typeof(object), typeof(TabControl), new PropertyMetadata(null));
+
+
+
+
+        public DataTemplate TabItemTemplate
+        {
+            get { return (DataTemplate)GetValue(TabItemTemplateProperty); }
+            set { SetValue(TabItemTemplateProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TabItemTemplate.  This enables animation, styling, binding, etc...
+        internal static DependencyProperty TabItemTemplateProperty { get; } =
+            DependencyProperty.Register("TabItemTemplate", typeof(DataTemplate), typeof(TabControl), new PropertyMetadata(null));
+
+
+        #endregion
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+        }
     }
 }
